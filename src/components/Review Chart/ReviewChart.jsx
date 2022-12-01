136b083236defaf318b1f24e9f809b84ab6469e4
Aankirz <sahuankit453@gmail.com>
2022-12-01 19:15:27 +0530

    dashboard added

---
+++ src/components/Review Chart/ReviewChart.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './ReviewChart.css'
+
+const ReviewChart = () => {
+  return (
+    <div className='ReviewChart'>
+       
+        
+  
+
+<iframe loading="lazy" 
+src="https://www.canva.com/design/DAFTg0T1Xrs/view?embed" >
+</iframe>
+
+  <iframe loading="lazy" 
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFTgzdopAY&#x2F;view?embed" >
+  </iframe>
+
+
+    </div>
+  )
+}
+
+export default ReviewChart
